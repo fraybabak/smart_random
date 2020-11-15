ecf7641b55b3c1433a5dc55b21b42752211f017f
babyjesus <you@example.com>
2020-11-15 23:06:22 +0330

    testing

--- src/actions/abundance.js
+++ src/actions/abundance.js
@@ -1,13 +1,14 @@
 module.exports.abundanceCalculator = (items) => {
   // items are objects
   let total = 0;
+  let abundances = [];
   for (i of Object.values(items)) {
     total = total + i;
   }
   // we are looping through each pair (keys,values) and pushing to abundances by (total-value)times
   for (let [key, value] of Object.entries(items)) {
     for (let i = 0; i < total - value; i++) {
-      abundances.push(key);
+      .push(key);
     }
   }
   // now we have an array of items with abundance , we can simply just pick from new array.
